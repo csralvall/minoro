9d5e26a98ca6b5f86eb8377a2a8d5c2b5d643f35
Cesar Alvarez Vallero <csralvarezvallero12@gmail.com>
2021-04-09 22:33:30 -0300

    changed text from span tag to text svg tag

--- script.js
+++ script.js
@@ -197,22 +197,17 @@ document.getElementById("countdown-container").innerHTML = `
   <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
     <g class="base-timer__circle">
       <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-      <path
+      <circle
         id="base-timer-path-remaining"
-        stroke-dasharray="283"
         class="base-timer__path-remaining ${remainingPathColor}"
-        d="
-          M 50, 50
-          m -45, 0
-          a 45,45, 0 1,0 90,0
-          a 45,45 0 1,0 -90,0
-        "
-      ></path>
+        stroke-dasharray="283"
+        cx="50" cy="50" r="45"></circle>
+        <text id="base-timer-label"
+          class="base-timer__label"
+          x="50%" y="50%"
+          dy=".4em">00:00</text>
     </g>
   </svg>
-  <span id="base-timer-label" class="base-timer__label">
-    00:00
-  </span>
 `
 
 const timer = roundTimer;
